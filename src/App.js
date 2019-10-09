e5df61404d5d94ce804716f813a26c8977c5d08b
Raka Pratama <auliya.rp@smooets.com>
2019-10-10 00:43:52 +0700

    Adding menu item

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React from 'react';
-import './App.css';
+import HomePage from './pages/homepage/homepage.component';
 
-function App() {
-  return <div>Hello World</div>
+import './App.css';
+class App extends React.Component {
+  render() {
+    return (
+      <div className='App'>
+        <HomePage />
+      </div>
+    );
+  }
 }
 
 export default App;
