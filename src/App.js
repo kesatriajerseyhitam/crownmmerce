66a057d315db43c973471f3f2425eb6f619b3d45
Raka Pratama <auliya.rp@smooets.com>
2019-10-16 23:52:51 +0700

    Adding header component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import Header from './components/header/header.component';
 import HomePage from './pages/homepage/homepage.component';
 import Shop from './pages/shop/shop.component';
 
@@ -10,6 +11,7 @@ import './App.css';
 const App = () => {
   return (
     <div className='App'>
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={Shop} />
